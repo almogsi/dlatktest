61ccb9b88ff3593f2f9546324f47291261e9a292
Almog Simchon <almog.si@gmail.com>
2021-01-19 14:49:47 +0200

    update

--- R/funcs.R
+++ R/funcs.R
@@ -22,102 +22,121 @@ predict_age <- function(x){
 }
 
 predict_open <- function(x){
+  #make tri-grams
+  open_dict$term <- stringr::str_replace_all(open_dict$term, " ", "_")
   #create weights
   vector_open<- open_dict$ope
   names(vector_open) <-  open_dict$term
 
   #create document-feature matrix of the dictionary
-  dfm_text <- dfm(tolower(x), select = open_dict$term, ngram = c(1:3),
-                  valuetype = "fixed")
+  toks <- tokens(x) %>%
+    tokens_tolower() %>% 
+    tokens_ngrams(n = 1:3)
+  
+  dfm_text <- dfm(toks, select = open_dict$term, valuetype = "fixed")
 
   #apply weights
-  dfm_text_wighted <- dfm_text %>%
-    dfm_weight(scheme = "boolean") %>%
-    dfm_weight(weights = vector_open, force = T)
-
-  #create a vector of opens and add the intercept of the model
+  dfm_text_wighted <- suppressWarnings({dfm_weight(dfm_text, weights = vector_open)})
+    
+  #create a vector of opens
   open <-  rowSums(dfm_text_wighted)
   names(open) <- NULL
-  return(scales::rescale(open, to = c(1,5)))
+  return(return(open))
 }
 
 predict_conc <- function(x){
+  #make tri-grams
+  conc_dict$term <- stringr::str_replace_all(conc_dict$term, " ", "_")
+  
   #create weights
   vector_conc<- conc_dict$con
   names(vector_conc) <-  conc_dict$term
 
   #create document-feature matrix of the dictionary
-  dfm_text <- dfm(tolower(x), select = conc_dict$term, ngram = c(1:3),
-                  valuetype = "fixed")
-
+  toks <- tokens(x) %>%
+    tokens_tolower() %>% 
+    tokens_ngrams(n = 1:3)
+  
+  dfm_text <- dfm(toks, select = conc_dict$term, valuetype = "fixed")
+  
   #apply weights
-  dfm_text_wighted <- dfm_text %>%
-    dfm_weight(scheme = "boolean") %>%
-    dfm_weight(weights = vector_conc, force = T)
-
-  #create a vector of concs and add the intercept of the model
+  dfm_text_wighted <- suppressWarnings({dfm_weight(dfm_text, weights = vector_conc)})
+  
+  #create a vector of concs
   conc <-  rowSums(dfm_text_wighted)
   names(conc) <- NULL
-  return(scales::rescale(conc, to = c(1,5)))
+  return(conc)
 }
 
 predict_ext <- function(x){
+  #make tri-grams
+  ext_dict$term <- stringr::str_replace_all(ext_dict$term, " ", "_")
+  
   #create weights
   vector_ext<- ext_dict$ext
   names(vector_ext) <-  ext_dict$term
-
+  
   #create document-feature matrix of the dictionary
-  dfm_text <- dfm(tolower(x), select = ext_dict$term, ngram = c(1:3),
-                  valuetype = "fixed")
-
+  toks <- tokens(x) %>%
+    tokens_tolower() %>% 
+    tokens_ngrams(n = 1:3)
+  
+  dfm_text <- dfm(toks, select = ext_dict$term, valuetype = "fixed")
+  
   #apply weights
-  dfm_text_wighted <- dfm_text %>%
-    dfm_weight(scheme = "boolean") %>%
-    dfm_weight(weights = vector_ext, force = T)
-
-  #create a vector of exts and add the intercept of the model
+  dfm_text_wighted <- suppressWarnings({dfm_weight(dfm_text, weights = vector_ext)})
+  
+ #create a vector of exts
   ext <-  rowSums(dfm_text_wighted)
   names(ext) <- NULL
-  return(scales::rescale(ext, to = c(1,5)))
+  return(ext)
 }
 
 predict_agr <- function(x){
+  #make tri-grams
+  agr_dict$term <- stringr::str_replace_all(agr_dict$term, " ", "_")
+  
   #create weights
   vector_agr<- agr_dict$agr
   names(vector_agr) <-  agr_dict$term
 
   #create document-feature matrix of the dictionary
-  dfm_text <- dfm(tolower(x), select = agr_dict$term, ngram = c(1:3),
-                  valuetype = "fixed")
-
+  toks <- tokens(x) %>%
+    tokens_tolower() %>% 
+    tokens_ngrams(n = 1:3)
+  
+  dfm_text <- dfm(toks, select = agr_dict$term, valuetype = "fixed")
+  
   #apply weights
-  dfm_text_wighted <- dfm_text %>%
-    dfm_weight(scheme = "boolean") %>%
-    dfm_weight(weights = vector_agr, force = T)
-
-  #create a vector of agrs and add the intercept of the model
+  dfm_text_wighted <- suppressWarnings({dfm_weight(dfm_text, weights = vector_agr)})
+  
+  #create a vector of agrs
   agr <-  rowSums(dfm_text_wighted)
   names(agr) <- NULL
-  return(scales::rescale(agr, to = c(1,5)))
+  return(agr)
 }
 
 predict_neu <- function(x){
+  #make tri-grams
+  neu_dict$term <- stringr::str_replace_all(neu_dict$term, " ", "_")
+  
   #create weights
   vector_neu<- neu_dict$neu
   names(vector_neu) <-  neu_dict$term
 
   #create document-feature matrix of the dictionary
-  dfm_text <- dfm(tolower(x), select = neu_dict$term, ngram = c(1:3),
-                  valuetype = "fixed")
-
+  toks <- tokens(x) %>%
+    tokens_tolower() %>% 
+    tokens_ngrams(n = 1:3)
+  
+  dfm_text <- dfm(toks, select = neu_dict$term, valuetype = "fixed")
+  
   #apply weights
-  dfm_text_wighted <- dfm_text %>%
-    dfm_weight(scheme = "boolean") %>%
-    dfm_weight(weights = vector_neu, force = T)
-
-  #create a vector of neus and add the intercept of the model
+  dfm_text_wighted <- suppressWarnings({dfm_weight(dfm_text, weights = vector_neu)})
+  
+  #create a vector of neus
   neu <-  rowSums(dfm_text_wighted)
   names(neu) <- NULL
-  return(scales::rescale(neu, to = c(1,5)))
+  return(neu)
 }
 
